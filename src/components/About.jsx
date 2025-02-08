aab7360c0825d56b0609a2e63fc9db17655cf046
SUNDARAVEL7203 <sundarvel.kasi@gmail.com>
2025-02-08 09:32:49 +0530

    about modified

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,13 +15,22 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-20">
-        I am an enthusiastic engineering graduate and fresher with a strong foundation in MERN Stack development. Passionate about the IT industry, I enjoy building dynamic web applications and designing engaging user interfaces. I recently completed a Full Stack project, Hotel Booking App, using the MERN Stack, which features real-time payment integration for secure transactions. With skills in React, Node.js, Express.js, and MongoDB, I am committed to continuous learning and honing my technical abilities. Dedicated, curious, and detail-oriented, I aim to contribute effectively to innovative projects while growing as a proficient software developer in the ever-evolving tech landscape.
+        I am an enthusiastic engineering graduate and fresher with a strong foundation in MERN Stack development.
+        Passionate about the IT industry, I enjoy building dynamic web applications and designing engaging user interfaces. 
+        I recently completed a Full Stack project, Hotel Booking App, using the MERN Stack, which features real-time payment 
+        integration for secure transactions. With skills in React, Node.js, Express.js, and MongoDB, I am committed to 
+        continuous learning and honing my technical abilities. Dedicated, curious, and detail-oriented, I aim to 
+        contribute effectively to innovative projects while growing as a proficient software developer in the 
+        ever-evolving tech landscape.
         </p>
 
         <br />
 
         <p className="text-xl">
-        As a detail-oriented individual, I value teamwork, problem-solving, and creating impactful digital solutions. My goal is to grow as a proficient software developer while contributing effectively to innovative projects in a fast-paced tech environment. I am eager to turn ideas into reality and bring value to the teams I collaborate with.
+        As a detail-oriented individual, I value teamwork, problem-solving, and creating impactful digital solutions.
+        My goal is to grow as a proficient software developer while contributing effectively to innovative projects 
+        in a fast-paced tech environment. I am eager to turn ideas into reality and bring value to the teams I collaborate 
+        with.
         </p>
       </div>
     </div>
